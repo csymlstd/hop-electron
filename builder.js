a42e6064ad849a93c6b05fbf5fcbae89708bc887
csymlstd <casey@caseymilstead.com>
2017-11-19 13:13:04 -0600

    init

---
+++ builder.js
@@ -0,0 +1,16 @@
+{
+  "osx" : {
+    "title": "Hop Kiosk",
+    "background": "assets/osx/background.png",
+    "icon": "assets/osx/logo.icns",
+    "icon-size": 128,
+    "contents": [
+      { "x": 355, "y": 125, "type": "link", "path": "/Applications" },
+      { "x": 155, "y": 125, "type": "file" }
+    ]
+  },
+  "win" : {
+    "title" : "Hop Kiosk",
+    "icon" : "assets/win/logo.ico"
+  }
+}
